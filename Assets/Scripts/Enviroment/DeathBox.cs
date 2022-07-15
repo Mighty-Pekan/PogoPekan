5ecd441316612d935a551370bf61133aefb1cee2
nico-bertoli <89839392+nico-bertoli@users.noreply.github.com>
2022-07-15 12:18:27 +0200

    Thorns rework

---
+++ Assets/Scripts/Enviroment/DeathBox.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathBox : MonoBehaviour
+{
+        private void OnCollisionEnter2D(Collision2D other) {
+
+        if (other.collider.gameObject.CompareTag("PlayerBody")) {
+            GameController.GameOver();
+        }
+    }
+}

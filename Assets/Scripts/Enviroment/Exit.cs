7a3c7c02985aa408a02d23a3917d1c6568468332
Pietro <pietro.digiovanni.dev@gmail.com>
2022-07-18 18:01:55 +0200

    fixed bug on Exit script

--- Assets/Scripts/Enviroment/Exit.cs
+++ Assets/Scripts/Enviroment/Exit.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Exit : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other) {
-        if(other.gameObject.tag == "PlayerBody")
+        if(other.gameObject.tag == "Player")
             GameController.LoadNextLevel();
     }
 }

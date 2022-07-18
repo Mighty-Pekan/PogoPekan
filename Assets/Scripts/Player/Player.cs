5d85e91b8c95fb87202d7f3db5f5ed1748da8615
nico-bertoli <89839392+nico-bertoli@users.noreply.github.com>
2022-07-18 16:28:10 +0200

    Trick detection reworked

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -22,7 +22,7 @@ public class Player : MonoBehaviour
     private void Update()
     {
         transform.Rotate(new Vector3(0, 0, -InputManager.GetRotation() * rotationSpeed * Time.deltaTime));
-        tricksDetector.registerRotation(transform.rotation.z);
+        tricksDetector.registerRotation(transform.rotation.eulerAngles.z);
 
        
     }

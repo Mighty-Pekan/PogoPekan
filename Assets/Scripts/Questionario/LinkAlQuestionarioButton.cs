afaafc4cad97c088e3748ff6595e44297ac653b7
Marco Trapani <108862666+MarcoTrapani@users.noreply.github.com>
2022-07-22 17:09:33 +0200

    Cambiato Link Questionario
    Ora il link è giusto

--- Assets/Scripts/Questionario/LinkAlQuestionarioButton.cs
+++ Assets/Scripts/Questionario/LinkAlQuestionarioButton.cs
@@ -5,6 +5,6 @@ using UnityEngine;
 public class LinkAlQuestionarioButton : MonoBehaviour
 {
     public void VaiAlQuestionario() {
-        Application.OpenURL("https://docs.google.com/forms/d/1Jdntp2Cx8-1V-_RIvJqM5gd-JBbO5aWKb7KaBevvvgw/prefill");
+        Application.OpenURL("https://forms.gle/bczhsuZ8mWBhsYrZ6");
     }
 }

9b681bcec9fbe2aef189c8fddd84adc521957052
nico-bertoli <89839392+nico-bertoli@users.noreply.github.com>
2022-07-22 10:52:40 +0200

    settaggio per questionario

--- Assets/Scripts/Questionario/LinkAlQuestionarioButton.cs
+++ Assets/Scripts/Questionario/LinkAlQuestionarioButton.cs
@@ -5,6 +5,6 @@ using UnityEngine;
 public class LinkAlQuestionarioButton : MonoBehaviour
 {
     public void VaiAlQuestionario() {
-        Application.OpenURL("https://www.natureconservancy.ca/fr/nos-actions/ressources/especes-en-vedette/mammiferes/pekan.html");
+        Application.OpenURL("https://docs.google.com/forms/d/1Jdntp2Cx8-1V-_RIvJqM5gd-JBbO5aWKb7KaBevvvgw/prefill");
     }
 }

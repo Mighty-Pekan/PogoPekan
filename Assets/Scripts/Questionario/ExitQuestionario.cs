d681e09abea89f7fbc9a58ab2d725b5fbf36a723
Pietro <pietro.digiovanni.dev@gmail.com>
2022-07-22 12:38:35 +0200

    questionario fix

--- Assets/Scripts/Questionario/ExitQuestionario.cs
+++ Assets/Scripts/Questionario/ExitQuestionario.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ExitQuestionario : MonoBehaviour
+public class ExitQuestionario : MonoSingleton<ExitQuestionario>
 {
     [SerializeField] GameObject QuestionarioPanel;
 

295ec7c9003e9a4c32d2edd77d5f9e06d60ed0f3
nico-bertoli <nicobertoli.1999@gmail.com>
2022-08-03 19:11:23 +0200

    aaa

--- Assets/Scripts/Enviroment/TraversablePlatform.cs
+++ Assets/Scripts/Enviroment/TraversablePlatform.cs
@@ -12,7 +12,9 @@ public class TraversablePlatform : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.transform.root.GetComponent<Rigidbody2D>().velocity.y > 0 || other.transform.position.y < transform.position.y)
+        if (other.gameObject.transform.root.GetComponent<Rigidbody2D>() != null && 
+            other.gameObject.transform.root.GetComponent<Rigidbody2D>().velocity.y > 0 ||
+            other.transform.position.y < transform.position.y)
             boxCollider.enabled = false;
         else
             boxCollider.enabled = true;

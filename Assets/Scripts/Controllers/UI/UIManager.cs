4f001cd042b307c9ae975ea3a842e446bbd2df42
Pietro <pietro.digiovanni.dev@gmail.com>
2022-08-03 23:54:39 +0200

    Pause panel, with return to main menu added

--- Assets/Scripts/Controllers/UI/UIManager.cs
+++ Assets/Scripts/Controllers/UI/UIManager.cs
@@ -1,7 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoSingleton<UIManager>
 {
+    [SerializeField] GameObject PausePanel;
+    [SerializeField] GameObject PauseButton;
+
+    private void Start()
+    {
+        PausePanel.SetActive(false);
+        PauseButton.SetActive(true);
+    }
+
+    public void OpenPausePanel(bool isOpen)
+    {
+        PausePanel.SetActive(isOpen);
+        PauseButton.SetActive(!isOpen);
+        GameController.instance.IsPause = isOpen;
+    }
+
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
 }

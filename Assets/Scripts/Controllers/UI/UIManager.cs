9f556b4a231e6b82f33cdc76432fff5c947f538a
Pietro <pietro.digiovanni.dev@gmail.com>
2022-08-05 17:56:08 +0200

    added funny text

--- Assets/Scripts/Controllers/UI/UIManager.cs
+++ Assets/Scripts/Controllers/UI/UIManager.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using TMPro;
 public class UIManager : MonoSingleton<UIManager>
 {
     [SerializeField] GameObject PausePanel;
     [SerializeField] GameObject PauseButton;
+    [SerializeField] TextMeshProUGUI FunnyText;
+
+    [SerializeField] List<string> funnyTexts = new List<string>();
 
     private void Start()
     {
@@ -23,11 +26,20 @@ public class UIManager : MonoSingleton<UIManager>
             PausePanel.SetActive(isOpen);
         if (PausePanel != null)
             PauseButton.SetActive(!isOpen);
+
         GameController.Instance.IsPause = isOpen;
+
+        FunnyText.text = GetRandomText();
+
     }
 
     public void ReturnToMainMenu()
     {
         SceneManager.LoadScene("Menu");
+    }
+
+    private string GetRandomText()
+    {
+        return funnyTexts[Random.Range(0, funnyTexts.Count)];
     }
 }

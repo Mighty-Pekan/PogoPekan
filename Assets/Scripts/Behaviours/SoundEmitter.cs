228856d0f08e745a5a613e64d80b8a529e7b25af
nico-bertoli <nicobertoli.1999@gmail.com>
2022-08-31 12:16:53 +0200

    ground sounds added

---
+++ Assets/Scripts/Behaviours/SoundEmitter.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEmitter : MonoBehaviour
+{
+    [SerializeField] AudioClip[] possibleSounds;
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.gameObject.tag == "Player") {
+            int r = Random.Range(0, possibleSounds.Length);
+            Debug.Log(r);
+            AudioManager.Instance.PlaySound(possibleSounds[r]);
+        }
+    }
+}

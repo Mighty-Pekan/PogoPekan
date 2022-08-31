c5a5924ab5c0bf3d9ee633fb85803ddf6bd46c08
nico-bertoli <nicobertoli.1999@gmail.com>
2022-08-31 23:16:30 +0200

    added breakable platforms particles

--- Assets/Scripts/Enviroment/BreakablePlatformButtHit.cs
+++ Assets/Scripts/Enviroment/BreakablePlatformButtHit.cs
@@ -6,48 +6,45 @@ public class BreakablePlatformButtHit : MonoBehaviour
 {
     int life = 4;
     int tickDamage = 1;
-    float tickRateo = 0f; //increase to break platform slower
+    //float tickRateo = 0f; //increase to break platform slower
 
     private SpriteRenderer mySpriteRenderer;
     private int quarterOfLife;
     private float? lastTimeTookDamage = null;
 
+    [SerializeField] GameObject destructionParticles;
+
     private void Start() {
         quarterOfLife = life / 4;
         mySpriteRenderer = GetComponent<SpriteRenderer>();
-        DecideColor();
+
+        GetComponent<BoxCollider2D>().size = new Vector2(mySpriteRenderer.size.x, mySpriteRenderer.size.y);
     }
 
 
     private void OnCollisionStay2D(Collision2D collision) {
-        if (collision.collider.gameObject.tag == "BouncingTip" && GameController.Instance.GetPlayer().IsPerformingButtHit())
-            TakeDamage();
-    }
-
-
-    public void TakeDamage()
-    {
-        if(tickRateo == 0f)Destroy(gameObject);
-
-        if (lastTimeTookDamage == null) lastTimeTookDamage = Time.time;
-        if (Time.time - lastTimeTookDamage > tickRateo)
-        {
-            lastTimeTookDamage = Time.time;
-            life -= tickDamage;
-
-            DecideColor();
-
-            if (life <= 0)
-            {
-                Destroy(gameObject);
-            }
+        if (collision.collider.gameObject.tag == "BouncingTip" && GameController.Instance.GetPlayer().IsPerformingButtHit()) {
+            //TakeDamage();
+            Instantiate(destructionParticles, collision.contacts[0].point,Quaternion.identity);
+            Destroy(gameObject);
         }
+
     }
 
-    private void DecideColor() {
-        if (life > 3 * quarterOfLife) mySpriteRenderer.color = Color.yellow;
-        else if (life > 2 * quarterOfLife) mySpriteRenderer.color = new Color(1, 0.6f, 0, 1);
-        else if (life > quarterOfLife) mySpriteRenderer.color = new Color(1, 0.3f, 0, 1); 
-        else mySpriteRenderer.color = Color.red;
-    }
+    //public void TakeDamage()
+    //{
+    //    if(tickRateo == 0f)Destroy(gameObject);
+
+    //    if (lastTimeTookDamage == null) lastTimeTookDamage = Time.time;
+    //    if (Time.time - lastTimeTookDamage > tickRateo)
+    //    {
+    //        lastTimeTookDamage = Time.time;
+    //        life -= tickDamage;
+
+    //        if (life <= 0)
+    //        {
+    //            Destroy(gameObject);
+    //        }
+    //    }
+    //}
 }

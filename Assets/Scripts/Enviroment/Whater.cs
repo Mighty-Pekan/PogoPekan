21c9b91813d13e4c94449d0e708445daabf9b6ec
nico-bertoli <nicobertoli.1999@gmail.com>
2022-09-01 12:22:43 +0200

    a

--- Assets/Scripts/Enviroment/Whater.cs
+++ Assets/Scripts/Enviroment/Whater.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Whater : MonoBehaviour
 {
     [SerializeField] private float timeToKill = 0.5f;
+    [SerializeField] private GameObject WhaterParticle;
     private float? enterTime = null;
 
     private void OnTriggerEnter2D(Collider2D collision) {
         enterTime = Time.time;
+        Instantiate(WhaterParticle, collision.gameObject.transform.position,Quaternion.identity);
     }
     private void OnTriggerExit2D(Collider2D collision) {
         enterTime = null;

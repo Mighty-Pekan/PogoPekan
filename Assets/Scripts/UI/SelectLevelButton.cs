86ebb4cdbc44ab86303afb6ad6df284708b036fb
nico-bertoli <nicobertoli.1999@gmail.com>
2022-09-01 22:37:46 +0200

    a

---
+++ Assets/Scripts/UI/SelectLevelButton.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectLevelButton : MonoBehaviour
+{
+    public void ReturnToLevelSelection() {
+
+    }
+}

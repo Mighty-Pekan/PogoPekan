d9c987d613e2daf66be41213f2fa4d42ea5dfb97
nico-bertoli <nicobertoli.1999@gmail.com>
2022-09-02 16:51:19 +0200

    better whater implementation

--- Assets/Scripts/Enviroment/Whater.cs
+++ Assets/Scripts/Enviroment/Whater.cs
@@ -4,26 +4,31 @@ using UnityEngine;
 
 public class Whater : MonoBehaviour
 {
-    [SerializeField] private float timeToKill = 0.5f;
     [SerializeField] private GameObject WhaterParticle;
     [SerializeField] private AudioClip WhaterSound;
-    private float? enterTime = null;
+
+    private bool playerHeadEntered;
+    private bool playerBottomEntered;
 
     private void OnTriggerEnter2D(Collider2D collision) {
-        if(collision.gameObject.tag == "Player") {
-            enterTime = Time.time;
+
+        if(collision.gameObject.tag == "PlayerHeadSpotter" || collision.gameObject.tag == "BouncingTip") {
             Instantiate(WhaterParticle, collision.gameObject.transform.position, Quaternion.identity);
             AudioManager.Instance.PlaySound(WhaterSound);
+            if(collision.gameObject.tag == "PlayerHeadSpotter")
+                playerHeadEntered = true;
+            else
+                playerBottomEntered = true;
+
+            if (playerHeadEntered && playerBottomEntered) GameController.Instance.GameOver();
         }
     }
     private void OnTriggerExit2D(Collider2D collision) {
-        if (collision.gameObject.tag == "Player") {
-            enterTime = null;
-        }
+        if (collision.gameObject.tag == "PlayerHeadSpotter")
+            playerHeadEntered = false;
+        else if (collision.gameObject.tag == "BouncingTip")
+            playerBottomEntered = false;
     }
-    private void OnTriggerStay2D(Collider2D collision) {
-        if(collision.gameObject.tag == "Player" && (Time.time - enterTime > timeToKill)) {
-            GameController.Instance.GameOver();
-        }
-    }
+
+
 }

5c0e9f42074c9f0d3198e15e16aa367aec23f619
nico-bertoli <nicobertoli.1999@gmail.com>
2022-09-02 15:08:53 +0200

    mushroom working

--- Assets/Scripts/Enviroment/Mushroom.cs
+++ Assets/Scripts/Enviroment/Mushroom.cs
@@ -6,6 +6,18 @@ public class Mushroom : MonoBehaviour
 {
     [SerializeField] float bounciness;
     [SerializeField] AudioClip BounceSound;
+    [SerializeField] float animationMaxScale = 12;
+    [SerializeField] float animationSpeed = 1f;
+
+    private bool DoingAnimation = false;
+    private float originalScale;
+
+    private void Start() {
+        originalScale = transform.localScale.x;
+
+        if (transform.localScale.x != transform.localScale.y)
+            Debug.LogWarning("mushroom x and y scale must be the same");
+    }
 
     public float GetBounciness() {
         return bounciness;
@@ -14,6 +26,27 @@ public class Mushroom : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision) {
         if(collision.gameObject.tag == "Player") {
             AudioManager.Instance.PlaySound(BounceSound);
+            StartCoroutine(DoAnimation());
+        }
+    }
+    private IEnumerator DoAnimation() {
+        if (!DoingAnimation) {
+            DoingAnimation = true;
+
+            while(transform.localScale.x < animationMaxScale) {
+                Debug.Log("transform local scale: "+transform.localScale.x);
+                float newVal = transform.localScale.x + animationSpeed;
+                transform.localScale = new Vector3(newVal,newVal,newVal);
+                yield return 0;
+            }
+
+            while(transform.localScale.x > originalScale) {
+                float newVal = transform.localScale.x - animationSpeed;
+                transform.localScale = new Vector3(newVal, newVal, newVal);
+                yield return 0;
+            }
+
+            DoingAnimation = false;
         }
     }
 

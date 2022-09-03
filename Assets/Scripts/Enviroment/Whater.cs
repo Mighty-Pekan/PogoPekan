1ce720e83fb946137d167bac51552ee4cfdb355d
Chaiko113 <108830634+Chaiko113@users.noreply.github.com>
2022-09-03 12:41:28 +0200

    Aggiunta animazione morte in acqua

--- Assets/Scripts/Enviroment/Whater.cs
+++ Assets/Scripts/Enviroment/Whater.cs
@@ -6,15 +6,27 @@ public class Whater : MonoBehaviour
 {
     [SerializeField] private GameObject WhaterParticle;
     [SerializeField] private AudioClip WhaterSound;
+    [SerializeField] private Sprite DeathSprite;
 
     private bool playerHeadEntered;
     private bool playerBottomEntered;
 
     private void OnTriggerEnter2D(Collider2D collision) {
 
-        if(collision.gameObject.tag == "PlayerHeadSpotter" || collision.gameObject.tag == "BouncingTip") {
-            Instantiate(WhaterParticle, collision.gameObject.transform.position, Quaternion.identity);
-            AudioManager.Instance.PlaySound(WhaterSound);
+        if (collision.gameObject.tag == "PlayerHeadSpotter" || collision.gameObject.tag == "BouncingTip")
+        {
+            if (playerBottomEntered == false && playerHeadEntered == false)
+            {
+                GameObject player = GameObject.Find("Player");
+                player.GetComponent<Rigidbody2D>().gravityScale = 0.3f;
+                player.GetComponent<Rigidbody2D>().velocity = new Vector2(player.GetComponent<Rigidbody2D>().velocity.x, player.GetComponent<Rigidbody2D>().velocity.y * 0.5f);
+                //player.GetComponent<Animator>().SetBool("IsDead", true);
+                player.GetComponent<Animator>().enabled = false;
+                GameObject.Find("Hammer").GetComponentInParent<Animator>().enabled = false;
+                player.GetComponent<SpriteRenderer>().sprite = DeathSprite;
+                Instantiate(WhaterParticle, collision.gameObject.transform.position, Quaternion.identity);
+                AudioManager.Instance.PlaySound(WhaterSound);
+            }
             if(collision.gameObject.tag == "PlayerHeadSpotter")
                 playerHeadEntered = true;
             else

ff9ab808b3b89640438cf99eb76d1c0551bfd25d
nico-bertoli <nicobertoli.1999@gmail.com>
2022-09-09 20:30:29 +0200

    quit button fixed

--- Assets/Scripts/UI/MainMenuUIManager.cs
+++ Assets/Scripts/UI/MainMenuUIManager.cs
@@ -6,9 +6,19 @@ public class MainMenuUIManager : MonoBehaviour
 {
     [SerializeField] GameObject MainMenuPanel;
     [SerializeField] GameObject LevelsSelectionPanel;
+    [SerializeField] AudioSource buttonClickAudioSource;
 
     private void Start() {
         MainMenuPanel.SetActive(!GameController.Instance.ShowLevelsPanel);
         LevelsSelectionPanel.SetActive(GameController.Instance.ShowLevelsPanel);
     }
+
+    public void MakeButtonSound() {
+        buttonClickAudioSource.Play();
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+
 }

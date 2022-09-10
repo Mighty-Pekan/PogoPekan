dd91d5f6441ff7a48309b7f2c37a7b3a87399d85
nico-bertoli <nicobertoli.1999@gmail.com>
2022-09-10 15:40:37 +0200

    whater reworked

--- Assets/Scripts/Enviroment/Whater.cs
+++ Assets/Scripts/Enviroment/Whater.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Whater : MonoBehaviour
 {
     [SerializeField] private GameObject WhaterParticle;
+    [SerializeField] private float whaterFallSpeed = 2f;
+    [SerializeField] private float xDeceleration = 5f;
     AudioSource audioSource;
 
     private bool playerHeadEntered;
@@ -16,15 +18,21 @@ public class Whater : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision) {
 
-        if(collision.gameObject.tag == "PlayerHeadSpotter" || collision.gameObject.tag == "BouncingTip") {
+        if (collision.gameObject.GetComponent<Player>()) {
             Instantiate(WhaterParticle, collision.gameObject.transform.position, Quaternion.identity);
             audioSource.Play();
+        }
+            
+
+        if (collision.gameObject.tag == "PlayerHeadSpotter" || collision.gameObject.tag == "BouncingTip") {
+
             if(collision.gameObject.tag == "PlayerHeadSpotter")
                 playerHeadEntered = true;
             else
                 playerBottomEntered = true;
 
             if (playerHeadEntered && playerBottomEntered) GameController.Instance.GameOver();
+
         }
     }
     private void OnTriggerExit2D(Collider2D collision) {
@@ -32,6 +40,14 @@ public class Whater : MonoBehaviour
             playerHeadEntered = false;
         else if (collision.gameObject.tag == "BouncingTip")
             playerBottomEntered = false;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision) {
+        Rigidbody2D otherRb = collision.gameObject.GetComponent<Rigidbody2D>();
+        if (otherRb) {
+            float newXSpeed = otherRb.velocity.x + (-Mathf.Sign(otherRb.velocity.x) * xDeceleration * Time.deltaTime);
+            otherRb.velocity = new Vector2(newXSpeed, -whaterFallSpeed);
+        }
     }
 
 
